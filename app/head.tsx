8d39e669897c4d19578a096a0254d4fa1a184aab
PaoloFromBru <paolo.falcioni@applia-europe.eu>
2025-07-19 16:31:21 +0200

    Use local icons for home screen

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,8 @@ export default function Head() {
   return (
     <>
       <link rel="manifest" href="/manifest.json" />
-      <link rel="icon" type="image/png" href="/icon-192.png" />
-      <link rel="apple-touch-icon" href="/icon-180.png" />
+      <link rel="icon" type="image/png" href="/android-chrome-192x192.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="theme-color" content="#008000" />
