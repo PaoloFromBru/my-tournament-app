66dae35c366fb97c7662fad266b5d23ae98fd460
PaoloFromBru <paolo.falcioni@applia-europe.eu>
2025-07-19 10:45:40 +0200

    Create head.tsx
    vediamo...

---
+++ app/head.tsx
@@ -0,0 +1,6 @@
+<link rel="manifest" href="/manifest.json" />
+<link rel="icon" type="image/png" href="/icon-192.png" />
+<link rel="apple-touch-icon" href="/icon-180.png" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+<meta name="theme-color" content="#008000" />
