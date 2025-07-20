c57f17f3bf506cc833f8e18735ca7da7a334e36a
PaoloFromBru <paolo.falcioni@applia-europe.eu>
2025-07-20 14:30:51 +0200

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <link rel="manifest" href="/manifest.json" />
-      <link rel="icon" type="image/png" href="/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" href="/new-192x192.png" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
